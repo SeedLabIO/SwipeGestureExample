1a72ad21d22246a7a96450aa2e5b5585a4f80661
Alen Liang <simplyaalen@gmail.com>
2016-07-22 10:46:38 +0800

    Initial Commit

---
+++ SwipeGestureExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwipeGestureExample
+//
+//  Created by LiangAlen on 22/07/16.
+//  Copyright © 2016 seedlab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwipeGestureExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwipeGestureExample
+//
+//  Created by LiangAlen on 22/07/16.
+//  Copyright © 2016 seedlab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
